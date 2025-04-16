2fde9a56eac96b0d37fb6f873601437006b97fee
nasrull97 <nasrull97nsll@gmail.com>
2025-04-17 00:38:18 +0700

    upload pertama

---
+++ js/main.js
@@ -0,0 +1,121 @@
+(function ($) {
+  "use strict";
+
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
+
+  // Initiate the wowjs
+  new WOW().init();
+
+  // Facts counter
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 2000,
+  });
+
+  // Typed Initiate
+  if ($(".typed-text-output").length == 1) {
+    var typed_strings = $(".typed-text").text();
+    var typed = new Typed(".typed-text-output", {
+      strings: typed_strings.split(", "),
+      typeSpeed: 100,
+      backSpeed: 20,
+      smartBackspace: false,
+      loop: true,
+    });
+  }
+
+  // Smooth scrolling to section
+  $(".btn-scroll").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      $("html, body").animate(
+        {
+          scrollTop: $(this.hash).offset().top - 0,
+        },
+        1500,
+        "easeInOutExpo"
+      );
+    }
+  });
+
+  // Skills
+  $(".skill").waypoint(
+    function () {
+      $(".progress .progress-bar").each(function () {
+        $(this).css("width", $(this).attr("aria-valuenow") + "%");
+      });
+    },
+    { offset: "80%" }
+  );
+
+  // Portfolio isotope and filter
+  var portfolioIsotope = $(".portfolio-container").isotope({
+    itemSelector: ".portfolio-item",
+    layoutMode: "fitRows",
+  });
+  $("#portfolio-flters li").on("click", function () {
+    $("#portfolio-flters li").removeClass("active");
+    $(this).addClass("active");
+
+    portfolioIsotope.isotope({ filter: $(this).data("filter") });
+  });
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    dots: true,
+    loop: true,
+    items: 1,
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  // Circular Progress
+  $(".circular-progress").each(function () {
+    const value = $(this).data("progress");
+    const color = $(this).data("color") || $(this).data("color") || "#00bfff"; // Warna dflt
+    const circle = $(this).find(".progress")[0];
+    const radius = circle.r.baseVal.value;
+    const circumference = 2 * Math.PI * radius;
+
+    circle.style.strokeDasharray = circumference;
+    circle.style.strokeDashoffset = circumference;
+    circle.style.stroke = color;
+
+    let currentProgress = 0;
+    const speed = 1;
+
+    function animateProgress() {
+      const offset = circumference - (currentProgress / 100) * circumference;
+      circle.style.strokeDashoffset = offset;
+
+      if (currentProgress < value) {
+        currentProgress++;
+        requestAnimationFrame(animateProgress);
+      }
+    }
+
+    animateProgress();
+  });
+})(jQuery);
